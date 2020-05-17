efcfbadec487cb53e001fca2709913ffd57f6499
saibhaskar24 <saibaaskar24091999@gmail.com>
2020-05-17 12:19:30 +0530

    Final

--- app.js
+++ app.js
@@ -1,30 +1,43 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
-const mongoose = require('mongoose');
-var passport = require('passport');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+var dishRouter = require('./routes/dishRouter');
+var promoRouter = require('./routes/promoRouter');
+var leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
+const favoriteRouter = require('./routes/favoriteRouter')
 var config = require('./config');
 
-var authenticate = require('./authenticate');
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+
 const Dishes = require('./models/dishes');
 
-const url = 'mongodb://localhost:27017/conFusion';
-const connect = mongoose.connect(url);
+var passport = require('passport');
+var authenticate = require('./authenticate');
+
+// Connection URL
+const url = config.mongoUrl;
+const connect = mongoose.connect(url, {
+    useMongoClient: true,
+    /* other options */
+  });
 
 connect.then((db) => {
     console.log("Connected correctly to server");
 }, (err) => { console.log(err); });
 
-
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// Secure traffic only
 app.all('*', (req, res, next) => {
   if (req.secure) {
     return next();
@@ -33,46 +46,61 @@ app.all('*', (req, res, next) => {
     res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
   }
 });
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(cookieParser('12345-67890-09876-54321'));
+
+// app.use(session({
+//   name: 'session-id',
+//   secret: '12345-67890-09876-54321',
+//   saveUninitialized: false,
+//   resave: false,
+//   store: new FileStore()
+// }));
+
 app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.session());
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var dishRouter = require('./routes/dishRouter');
-const uploadRouter = require('./routes/uploadRouter');
-// var promoRouter = require('./routes/promoRouter');
-// var leaderRouter = require('./routes/leaderRouter');
+app.use('/', index);
+app.use('/users', users);
 
+// function auth (req, res, next) {
+//   console.log(req.user);
 
-// app.use(cookieParser('12345-67890-09876-54321'));
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
+//   if (!req.user) {
+//     var err = new Error('You are not authenticated!');
+//     res.setHeader('WWW-Authenticate', 'Basic');                          
+//     err.status = 401;
+//     next(err);
+//   }
+//   else {
+//         next();
+//   }
+// }
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-// const url = config.mongoUrl;
+// app.use(auth);
 
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes',dishRouter);
+app.use('/promotions',promoRouter);
+app.use('/leaders',leaderRouter);
 app.use('/imageUpload',uploadRouter);
-// app.use('/promotions',promoRouter);
-// app.use('/leaders',leaderRouter);
+app.use('/favorites',favoriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
