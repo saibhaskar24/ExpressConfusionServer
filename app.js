82c5ab6999a052653585f18a1f1cc081a66d5a49
saibhaskar24 <saibaaskar24091999@gmail.com>
2020-05-17 09:20:14 +0530

    HTTPS

--- app.js
+++ app.js
@@ -25,7 +25,14 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+  else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
